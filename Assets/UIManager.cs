ae38be4efc43c4653f9ba5e7368c3232fa39eb0d
sandwich189 <46155245+sandwich189@users.noreply.github.com>
2019-04-27 18:22:28 +0800

    Added UI

---
+++ Assets/UIManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    private GameObject ui;
+
+    private GameObject resourceHolder;
+    private TextMeshProUGUI txt_wood;
+    private TextMeshProUGUI txt_stone;
+    private TextMeshProUGUI txt_iron;
+    private TextMeshProUGUI txt_gold;
+
+    private GameObject woodHolder;
+    private GameObject stoneHolder;
+    private GameObject ironHolder;
+    private GameObject goldHolder;
+
+    public int ui_wood = 0;
+    public int ui_stone = 0;
+    public int ui_iron = 0;
+    public int ui_gold = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ui = this.gameObject.transform.GetChild(0).gameObject;
+        resourceHolder = ui.transform.GetChild(0).gameObject;
+
+        woodHolder = resourceHolder.transform.GetChild(0).gameObject;
+        stoneHolder = resourceHolder.transform.GetChild(1).gameObject;
+        ironHolder = resourceHolder.transform.GetChild(2).gameObject;
+        goldHolder = resourceHolder.transform.GetChild(3).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt_wood = woodHolder.GetComponent<TextMeshProUGUI>();
+        txt_wood.text = "Wood: " + ui_wood;
+
+        txt_stone = stoneHolder.GetComponent<TextMeshProUGUI>();
+        txt_stone.text = "Stone: " + ui_stone;
+
+        txt_iron = ironHolder.GetComponent<TextMeshProUGUI>();
+        txt_iron.text = "Iron: " + ui_iron;
+
+        txt_gold = goldHolder.GetComponent<TextMeshProUGUI>();
+        txt_gold.text = "Gold: " + ui_gold;
+    }
+}
--- Assets/resouces.cs
+++ Assets/resouces.cs
@@ -6,11 +6,57 @@ public class resouces : MonoBehaviour
 {
     public int hp = 1;
 
+    public bool wood = true, stone = false, iron = false, gold = false;
+    private int resourcetype;
+
+    private void Start()
+    {
+        if (wood)
+        {
+            resourcetype = 0;
+        }
+
+        if (stone)
+        {
+            resourcetype = 1;
+        }
+
+        if (iron)
+        {
+            resourcetype = 2;
+        }
+
+        if (gold)
+        {
+            resourcetype = 3;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (hp <= 0)
         {
+            switch (resourcetype)
+
+            {
+            case 0:
+                GameObject.Find("UIManager").GetComponent<UIManager>().ui_wood += 1;
+                break;
+
+            case 1:
+                GameObject.Find("UIManager").GetComponent<UIManager>().ui_stone += 1;
+                break;
+
+            case 2:
+                GameObject.Find("UIManager").GetComponent<UIManager>().ui_iron += 1;
+                break;
+
+            case 3:
+                GameObject.Find("UIManager").GetComponent<UIManager>().ui_gold += 1;
+                break;
+            }
+
             GameObject.Find("LevelGenerator").GetComponent<LevelGenerator>().curSpawn -= 1;
             Destroy(gameObject);
         }
